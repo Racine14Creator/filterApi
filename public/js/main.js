8fe6efe6ca4b12eecb5b696790002afb4dfa74df
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2023-12-28 15:20:09 +0200

    style

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+
+const display = document.getElementById("result"),
+    inputSearch = document.getElementById('search'),
+    urlAPIs = "https://jsonplaceholder.typicode.com/users"
+
+const getData = async () => {
+    const res = await fetch(urlAPIs),
+        data = await res.json()
+    return data
+}
+const displayFunction = async () => {
+    let q = inputSearch.value
+    const payload = await getData()
+    let displayData = payload.filter(event => {
+        if (q === '') { return event }
+        else if (event.name.toLowerCase().includes(q.toLowerCase())) { return event }
+    }).map(object => {
+        const { name, id, username } = object
+        return `
+                <div class="item" id=${id}>
+                    <h3>${name}</h3>
+                    <p>${username}</p>
+                </div>
+            `
+    }).join("")
+    display.innerHTML = displayData
+}
+displayFunction()
+inputSearch.addEventListener('input', function () {
+    displayFunction()
+})
+
